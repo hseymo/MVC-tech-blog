755791fc4d52f30d688f09eaecfa29b4c3e61fd0
Haley Seymour <haleycseymour@comcast.net>
2022-05-09 22:44:06 -0700

    delete button

--- public/js/updateDelete.js
+++ public/js/updateDelete.js
@@ -23,25 +23,15 @@ document.querySelector("#update").addEventListener("click",event=>{
     })
 })
 
-document.querySelector("#update").addEventListener("click",event=>{
+document.querySelector("#delete").addEventListener("click",event=>{
     event.preventDefault();
     const blogId = document.querySelector("#hiddenBlogId").value;
-    const editBlog = {
-        title:document.querySelector("#editedTitle").value,
-        content:document.querySelector("#editedContent").value,
-    }
-    console.log(blogId);
-    console.log(editBlog);
     fetch((`/api/blogs/${blogId}`),{
-        method:"POST",
-        body:JSON.stringify(editBlog),
-        headers:{
-            "Content-Type":"application/json"
-        }
+        method:"DELETE",
     }).then(res=>{
         if(res.ok){
-            console.log("blog updated")
-            location.reload()
+            console.log("blog deleted")
+            location.href="/dashboard"
         } else {
             alert("please try again")
         }
